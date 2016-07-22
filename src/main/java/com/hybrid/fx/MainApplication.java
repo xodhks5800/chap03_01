61def0ccb4512b2d3c471736e2b811aded9cd87d
java <java>
2016-07-22 17:29:34 +0900

    GluonDesktop add..

---
+++ src/main/java/com/hybrid/fx/MainApplication.java
@@ -0,0 +1,23 @@
+package com.hybrid.fx;
+
+import com.gluonhq.particle.application.ParticleApplication;
+import javafx.scene.Scene;
+import static org.controlsfx.control.action.ActionMap.actions;
+
+public class MainApplication extends ParticleApplication {
+
+    public MainApplication() {
+        super("chap03_01 Application");
+    }
+
+    @Override
+    public void postInit(Scene scene) {
+        scene.getStylesheets().add(MainApplication.class.getResource("style.css").toExternalForm());
+
+        setTitle("chap03_01 Application");
+
+        getParticle().buildMenu("File -> [exit]", "Help -> [about]");
+        
+        getParticle().getToolBarActions().addAll(actions("---", "about", "exit"));
+    }
+}
